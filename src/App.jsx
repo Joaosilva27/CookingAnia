99b7914576cfbcb9c5c4a91f325892eb9d88bbd9
Joaosilva27 <joaosilva7875@gmail.com>
2025-05-04 18:17:25 +0200

    added favicon & minor css changes
    * added cooking_mama icon to new "icons" folder
    
    * set cooking_mama.png as website's favicon
    
    * few css updates to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import CookingMamaIcon from "./icons/cooking_mama.png";
 
 function App() {
-  return <div className='text-4xl'>cooking ania</div>;
+  return (
+    <div className='p-4'>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-4xl'>Cooking Ania</h1>
+        <img src={CookingMamaIcon} className='h-20 ml-4' />
+      </div>
+    </div>
+  );
 }
 
 export default App;
