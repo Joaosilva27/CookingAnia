756b5c79463cdb1f9a2ebcd639bccbba4b41bf15
Joaosilva27 <joaosilva7875@gmail.com>
2025-05-05 16:59:35 +0200

    new RecipeContainer
    * new RecipeContainer component taking two props, title and images. Already fully styled using tailwind
    
    * new mealRecipes array of objects in MealsPage.jsx
    
    * mapping through mealRecipes, rendering for each recipe RecipeContainer and passing its respective props is fully working

--- src/pages/MealsPage.jsx
+++ src/pages/MealsPage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
-import Header from "../components/Header";
+import RecipeContainer from "../components/RecipeContainer";
+import MisoSalmonPicture from "../recipeImages/salmon_miso.jpg";
 
 export default function MealsPage() {
-  return <div></div>;
+  const mealRecipes = [
+    { id: 1, title: "Test", src: MisoSalmonPicture },
+    { id: 2, title: "Test", src: MisoSalmonPicture },
+  ];
+
+  return (
+    <div className='flex'>
+      {mealRecipes.map(recipe => (
+        <RecipeContainer id={recipe.id} title={recipe.title} image={recipe.src} />
+      ))}
+    </div>
+  );
 }
