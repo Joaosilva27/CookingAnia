98a0af21c63772a207fccb27c26fbd2e823d2af8
Joaosilva27 <joaosilva7875@gmail.com>
2025-05-06 16:56:48 +0200

    added a lot of meal recipes
    * added lots of new meal recipe pictures to recipeImages folder
    
    * rendered respective images and title on MealsPage.jsx
    
    * also added one dessert

--- src/pages/DessertPage.jsx
+++ src/pages/DessertPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import RecipeContainer from "../components/RecipeContainer";
 import CheesecakePicture from "../recipeImages/cheesecake.jpg";
+import MangoStickyRicePicture from "../recipeImages/mango_sticky_rice.jpg";
+import BananaBreadPicture from "../recipeImages/banana_bread.jpg";
 
 export default function DessertPage() {
-  const dessertRecipes = [{ title: "Basque Cheesecake", src: CheesecakePicture }];
+  const dessertRecipes = [
+    { title: "Basque Cheesecake", src: CheesecakePicture },
+    { title: "Mango Sticky Rice", src: MangoStickyRicePicture },
+    { title: "Banana Bread", src: BananaBreadPicture },
+  ];
 
   return (
     <div className='pt-4 md:pl-15 md:pr-15'>
--- src/pages/MealsPage.jsx
+++ src/pages/MealsPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import RecipeContainer from "../components/RecipeContainer";
 import MisoSalmonPicture from "../recipeImages/salmon_miso.jpg";
 import GarlicMirinChickenPicture from "../recipeImages/garlic_mirin_chicken.jpg";
+import RamenPicture from "../recipeImages/ramen.jpg";
 import BibimbapPicture from "../recipeImages/bibimbap.jpg";
 import TurkishEggsPicture from "../recipeImages/turkish_boiled_eggs.jpg";
 import StirFryUdonPicture from "../recipeImages/stir_fry_udon.jpg";
@@ -10,11 +11,21 @@ import HainaneseChickenPicture from "../recipeImages/hainanese_chicken_rice.jpg"
 import FeijoadaPicture from "../recipeImages/feijoada.jpg";
 import StirFryBeefRicePicture from "../recipeImages/stir_fry_beef_rice.jpg";
 import PolishCabaggeRollsPicture from "../recipeImages/polish_cabagge_rolls.jpg";
+import FishTacosPicture from "../recipeImages/fish_tacos.jpg";
+import KoreanColdNoodlesPicture from "../recipeImages/korean_cold_noodles.jpg";
+import ChickenKatsuCurryPicture from "../recipeImages/chicken_katsu_curry.jpg";
+import OvenRoastedSalmonPicture from "../recipeImages/oven_roasted_salmon.jpg";
+import PokeBowlPicture from "../recipeImages/pokebowl.jpg";
+import PolishInspiredKatsuChickenPicture from "../recipeImages/polish_inspired_chicken_katsu.jpg";
+import RedCurryChickenPicture from "../recipeImages/red_curry_chicken.jpg";
+import TunaBeanQuesadillaPicture from "../recipeImages/tuna_and_beans_quesadilla.jpg";
+import EggPattiesPicture from "../recipeImages/egg_patties_fries_salad.jpg";
 
 export default function MealsPage() {
   const mealRecipes = [
     { title: "Miso Salmon", src: MisoSalmonPicture },
     { title: "Garlic Mirin Chicken", src: GarlicMirinChickenPicture },
+    { title: "Ramen", src: RamenPicture },
     { title: "Bibimbap", src: BibimbapPicture },
     { title: "Turkish Soft Boiled Eggs", src: TurkishEggsPicture },
     { title: "Stir Fry Udon", src: StirFryUdonPicture },
@@ -22,6 +33,16 @@ export default function MealsPage() {
     { title: "Hainanese Chicken Rice", src: HainaneseChickenPicture },
     { title: "Feijoada Moda Anna", src: FeijoadaPicture },
     { title: "Stir Fry Beef with Rice", src: StirFryBeefRicePicture },
+    { title: "Polish Cabagge Rolls", src: PolishCabaggeRollsPicture },
+    { title: "Fish Tacos", src: FishTacosPicture },
+    { title: "Tuna and Beans Quesadilla", src: TunaBeanQuesadillaPicture },
+    { title: "Korean Cold Noodles", src: KoreanColdNoodlesPicture },
+    { title: "Chicken Katsu Curry", src: ChickenKatsuCurryPicture },
+    { title: "Oven Roasted Salmon", src: OvenRoastedSalmonPicture },
+    { title: "Poké Bowl", src: PokeBowlPicture },
+    { title: "Polish Inspired Katsu Chicken", src: PolishInspiredKatsuChickenPicture },
+    { title: "Red Curry Chicken", src: RedCurryChickenPicture },
+    { title: "Egg Patties with Fries and Salad", src: EggPattiesPicture },
   ];
 
   return (
